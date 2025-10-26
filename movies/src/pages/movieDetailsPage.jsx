2285b632f4377359e5835cc846602c49ab7c4ed4
Conan Quilty <conanhq03@gmail.com>
2025-10-26 17:43:03 +0000

    Created cast system (For displaying actors): movieCast component

--- movies/src/pages/movieDetailsPage.jsx
+++ movies/src/pages/movieDetailsPage.jsx
@@ -2,27 +2,45 @@ import React from "react";
 import { useParams } from 'react-router';
 import MovieDetails from "../components/movieDetails/";
 import PageTemplate from "../components/templateMoviePage";
-import { getMovie } from '../api/tmdb-api'
+import { getMovie, getMovieCast } from '../api/tmdb-api'
 import { useQuery } from '@tanstack/react-query';
 import Spinner from '../components/spinner'
 // import useMovie from "../hooks/useMovie";   Redundant
+import MovieCast from "../components/movieCast";
 
 
 const MoviePage = (props) => {
   const { id } = useParams();
-    const { data: movie, error, isPending, isError  } = useQuery({
+    const { data: movie, errorMovie, isPendingMovie, isErrorMovie  } = useQuery({
     queryKey: ['movie', {id: id}],
     queryFn: getMovie,
   })
 
-  if (isPending) {
+  if (isPendingMovie) {
     return <Spinner />;
   }
 
-  if (isError) {
-    return <h1>{error.message}</h1>;
+  if (isErrorMovie) {
+    return <h1>{errorMovie.message}</h1>;
   }
 
+  const {data: credits, errorCredits, isPendingCredits, isErrorCredits } = useQuery({
+    queryKey: ["cast", {id: id}],
+    queryFn: getMovieCast,
+  })
+  
+  console.log("Credits data:", credits);
+
+
+  if(isPendingCredits){
+    return <Spinner />
+  }
+
+  if (isErrorCredits) {
+    return <h1>{errorCredits.message}</h1>
+  }
+
+  const cast = credits?.cast?.slice(0, 10) || [] ;
 
   return (
     <>
@@ -30,6 +48,7 @@ const MoviePage = (props) => {
         <>
           <PageTemplate movie={movie}>
             <MovieDetails movie={movie} />
+            <MovieCast cast={cast} />
           </PageTemplate>
         </>
       ) : (
